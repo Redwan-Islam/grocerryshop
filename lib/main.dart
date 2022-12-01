fa8d57e8ee3678c5b8af9c1a0aebcfb1754cc66c
redwan-islam <redwanislamdip@outlook.com>
2022-12-01 12:44:16 +0600

    grocerryshop

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:grocerryshop/pages/splash_Page.dart';
+
+void main(List<String> args) {
+  runApp(myapp());
+}
+
+class myapp extends StatefulWidget {
+  const myapp({super.key});
+
+  @override
+  State<myapp> createState() => _myappState();
+}
+
+class _myappState extends State<myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: splashPage(),
+    );
+  }
+}
---
+++ lib/pages/splash_Page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class splashPage extends StatefulWidget {
+  const splashPage({super.key});
+
+  @override
+  State<splashPage> createState() => _splashPageState();
+}
+
+class _splashPageState extends State<splashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
